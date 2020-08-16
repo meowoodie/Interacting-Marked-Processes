2d0a7e3b8ee21df61ce994a6becbbb0eb907d58b
meowoodie <woodielove@163.com>
2020-08-16 00:35:33 -0400

    update data preprocess

--- anl/maoutage.py
+++ anl/maoutage.py
@@ -11,25 +11,35 @@ from tqdm import tqdm
 # read data into list
 data     = []
 counter  = 0
-rootpath = "/Users/woodie/Desktop/maoutagex"
+rootpath = "/Users/woodie/Desktop/outagex/maoutagex-2018"
 print("[%s] reading data into data frame ..." % arrow.now())
 for filename in tqdm(os.listdir(rootpath)):
     if filename.endswith(".h5"):
         f = h5py.File(os.path.join(rootpath, filename), "r")
         t = arrow.get(filename, "YYYYMMDDHHmm")
-        outage = np.array(f["outage"]).tolist()
-        idx    = np.array(f["idx"])
-        lat    = np.array(f["lat"])
-        lon    = np.array(f["lon"])
-        loc    = np.stack([lat, lon, idx], axis=1)
+        outage  = np.array(f["outage"])
+        idx     = np.array(f["idx"])
+        lat     = np.array(f["lat"])
+        lon     = np.array(f["lon"])
+        loc     = np.stack([lat, lon, idx], axis=1)
+        # sort by index
+        order   = idx.argsort()
+        idx     = idx[order]
+        outage  = outage[order]
+        loc     = loc[order, :]
+        # remove duplicate entries
+        _, _idx = np.unique(idx, return_index=True)
+        loc     = loc[_idx, :]
+        outage  = outage[_idx].tolist()
         # only keep complete data entry
-        if len(outage) == 371: 
+        if len(outage) == 351: 
             data.append([t.timestamp] + outage)
 
-np.save("data/geolocation_new.npy", loc)
+np.save("data/geolocation_351.npy", loc)
 
 print("[%s] sorting the list by their timestamp ..." % arrow.now())
 data = np.array(data)
+print(data.shape)
 data = data[np.argsort(data[:, 0])]
 print(data[0, 0])
 print(arrow.get(data[0, 0]))
@@ -41,8 +51,8 @@ start_t    = data[0, 0]  # start time
 end_t      = data[-1, 0] # end time
 data[:, 0] = (data[:, 0] - start_t) / (15 * 60)     # every 15 mins per event
 N          = int((end_t - start_t) / (15 * 60)) + 1 # total number of events
-mat        = np.zeros((N, 371), np.int32)
+mat        = np.zeros((N, 351), np.int32)
 for di, mi in tqdm(enumerate(data[:, 0])):
     mat[mi, :] = data[di, 1:]
 
-np.save("data/maoutage_new.npy", mat)
+np.save("data/maoutage_2018.npy", mat)
--- anl/weather.py
+++ anl/weather.py
@@ -8,53 +8,60 @@ import numpy as np
 import scipy.io as sio
 from tqdm import tqdm
 
-fk = "016"
 
-# read data into list
-data     = []
-times    = []
-counter  = 0
-rootpath = "/Users/woodie/Desktop/maweather"
-print("[%s] reading data into data frame ..." % arrow.now())
-for foldername in tqdm(os.listdir(rootpath)):
-    if foldername == ".DS_Store":
-        continue
-    for filename in os.listdir(os.path.join(rootpath, foldername)):
-        if not filename.endswith(".h5"):
+def preproc_weather(fk, rootpath, savepath):
+    # read data into list
+    data     = []
+    times    = []
+    counter  = 0
+    print("[%s] reading data into data frame ..." % arrow.now())
+    for foldername in tqdm(os.listdir(rootpath)):
+        if foldername == ".DS_Store":
             continue
-        # print(foldername, filename)
-        f = h5py.File(os.path.join(rootpath, foldername, filename), "r")
-        t = arrow.get(foldername[:8] + filename[1:3] + "0000", "YYYYMMDDHHmm")
-        # keys  = list(f.keys())
-        # featk = keys[:131]
-        # feat  = np.stack([ np.array(f[k]) for k in featk ], axis=1) # [ nlocations, nfeat ]
-        feat  = np.array(f[fk])              # [ nlocations ]
-        lat   = np.array(f["lat"])           # [ nlocations ]
-        lon   = np.array(f["lon"])           # [ nlocations ]
-        loc   = np.stack([lat, lon], axis=1) # [ nlocations, 2 ]
-        times.append(t.timestamp)
-        data.append(feat)
+        for filename in os.listdir(os.path.join(rootpath, foldername)):
+            if not filename.endswith(".h5"):
+                continue
+            # print(foldername, filename)
+            f = h5py.File(os.path.join(rootpath, foldername, filename), "r")
+            t = arrow.get(foldername[:8] + filename[1:3] + "0000", "YYYYMMDDHHmm")
+            # keys  = list(f.keys())
+            # featk = keys[:131]
+            # feat  = np.stack([ np.array(f[k]) for k in featk ], axis=1) # [ nlocations, nfeat ]
+            feat  = np.array(f[fk])              # [ nlocations ]
+            lat   = np.array(f["lat"])           # [ nlocations ]
+            lon   = np.array(f["lon"])           # [ nlocations ]
+            loc   = np.stack([lat, lon], axis=1) # [ nlocations, 2 ]
+            times.append(t.timestamp)
+            data.append(feat)
 
-np.save("data/weathergeolocations.npy", loc)
+    # np.save("data/weathergeolocations.npy", loc)
 
-print("[%s] sorting the list by their timestamp ..." % arrow.now())
-data  = np.stack(data, axis=0)
-# print(data.shape)
-data  = data[np.argsort(times)]
-times = np.array(times)
-times = times[np.argsort(times)]
-print(data.shape)
+    print("[%s] sorting the list by their timestamp ..." % arrow.now())
+    data  = np.stack(data, axis=0)
+    # print(data.shape)
+    data  = data[np.argsort(times)]
+    times = np.array(times)
+    times = times[np.argsort(times)]
+    print(data.shape)
 
-print("[%s] constructing complete data matrix as a data file ..." % arrow.now())
-start_t = times[0]  # start time
-end_t   = times[-1] # end time
-inds    = (times - start_t) / (60 * 60)     # every 60 mins per event
-print(inds)
-N       = int((end_t - start_t) / (60 * 60)) + 1 # total number of events
-K       = data.shape[1]
-mat     = np.zeros((N, K), np.float32)
-for di, mi in tqdm(enumerate(inds)):
-    mat[int(mi), :] = data[di]
-print(mat.shape)
+    print("[%s] constructing complete data matrix as a data file ..." % arrow.now())
+    start_t = times[0]  # start time
+    end_t   = times[-1] # end time
+    inds    = (times - start_t) / (60 * 60)     # every 60 mins per event
+    N       = int((end_t - start_t) / (60 * 60)) + 1 # total number of events
+    K       = data.shape[1]
+    mat     = np.zeros((N, K), np.float32)
+    for di, mi in tqdm(enumerate(inds)):
+        mat[int(mi), :] = data[di]
+    print(mat.shape)
 
-np.save("data/maweather-feat%s.npy" % fk, mat)
+    np.save(savepath, mat)
+
+if __name__ == "__main__":
+    fk_list = [ "001", "002", "003", "004", "005", "006", "007", "008", "009", "010", "011", "012", "013", "014", "015", "016", "017" ]
+
+    for fk in fk_list:
+        preproc_weather(
+            fk=fk, 
+            rootpath="/Users/woodie/Desktop/maweather/h5cache_MA_201803_p", 
+            savepath="data/maweather-201803/maweather-201803-feat%s.npy" % fk)
